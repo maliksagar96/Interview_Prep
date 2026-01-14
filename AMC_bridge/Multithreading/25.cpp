92b5bcbe423a1a1fd57deb1748bea51164f139c7
maliksagar96 <malik.sagar97@gmail.com>
2026-01-14 23:00:06 +0530

    Started Computatinal Geometry

---
+++ AMC_bridge/Multithreading/25.cpp
@@ -0,0 +1,13 @@
+/*
+25. What are conditional variables?
+
+Ans . Conditional variables (or condition variables) are synchronization primitives that allow threads to 
+      wait until a specific condition becomes true.  
+
+      Features
+      wait()
+      notify_one()
+      notify_all()
+
+
+*/
